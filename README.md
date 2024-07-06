a988e0d8af787463cfb354f6d33377b08eb9707a
Raien Elliston <118232707+only1Second@users.noreply.github.com>
2024-07-06 14:26:17 -0700

    got movment and gravity working

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Platformer-Game
+
+## Credits
+
+Tilemap - By Pixel Frog on itch.io https://pixelfrog-assets.itch.io/pixel-adventure-1
---
+++ Scenes/CharacterBody.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+@export var speed = 400
+@export var gravity = 20
+@export var jump_power = -800
+
+func _physics_process(delta):
+	velocity.x = 0
+	
+	if Input.is_action_pressed("right"):
+		velocity.x += speed
+	if Input.is_action_pressed("left"):
+		velocity.x -= speed
+	
+	if not is_on_floor():
+		velocity.y += gravity
+	if Input.is_action_just_pressed("up"):
+		velocity.y += jump_power
+		
+	move_and_slide()
+	move_and_collide(Vector2(0, gravity))
