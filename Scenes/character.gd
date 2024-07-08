10828be8b0700fc2e91836cfd454feaa9a63aa15
Raien Elliston <118232707+only1Second@users.noreply.github.com>
2024-07-08 01:35:21 -0700

    fixed light and added color reltated tiles now

--- Scenes/character.gd
+++ Scenes/character.gd
@@ -8,15 +8,14 @@ extends CharacterBody2D
 @onready var color_sprite: Sprite2D = $Sprite2D
 @onready var texture_rect: TextureRect = $Sprite2D/TextureRect
 @onready var point_light_2d = $LightNode/PointLight2D
-@onready var light_node = $LightNode
+@onready var light_node = $PointLight2D
 @onready var hidden_node = $HiddenNode
 
-
-enum CharacterColors { Red, Blue, Purple, Yellow, Green, White, Black, NONE }
+enum CharacterColors { Red, Orange, Yellow, Green, Blue, Purple, White, Black, NONE }
 @export var starting_color: CharacterColors 
 var character_color: CharacterColors = CharacterColors.NONE
 var jump_power = defualt_jump_power
-
+var bouncy = false
 
 # Color Asssistance Variables
 var red_emitting := false
@@ -46,46 +45,72 @@ func update_color() -> void:
 	
 	match character_color:
 		CharacterColors.Red: # Become a power source (like redstone)
-			point_light_2d.show()
+			bouncy = false
+			gravity = 20
+			light_node.visible = false
 			red_emitting = true
 			self.collision_mask = 1 | 2 | 3 
 			jump_power = -2000
 			color_sprite.modulate = Color("red")
 			
-		CharacterColors.Blue: # Passthrough blue + blue related objects
+		CharacterColors.Orange: # Passthrough blue + blue related objects
+			bouncy = true
+			gravity = 20
+			light_node.visible = false
 			red_emitting = false
-			self.collision_mask = 1 | 2
+			self.collision_mask = 1 | 2 | 3
 			jump_power = -2000
 			color_sprite.modulate = Color("blue")
 			
-		CharacterColors.Purple: # Passthrough with purple + purple related (intentionally confuse with blue)
-			red_emitting = false
-			self.collision_mask = 1 | 3
-			jump_power = -2000
-			color_sprite.modulate = Color("Purple")
-			
-		CharacterColors.Yellow:
+		CharacterColors.Yellow: # Bouncy. Gives character uncontrollable bouncy movment
+			bouncy = false
+			gravity = -20
+			light_node.visible = false
 			red_emitting = false
 			self.collision_mask = 1 | 2 | 3
 			jump_power = -2000
 			color_sprite.modulate = Color("yellow")
 			
 		CharacterColors.Green: # Increased jump value
+			bouncy = false
+			gravity = 20
+			light_node.visible = false
 			red_emitting = false
 			self.collision_mask = 1 | 2 | 3
 			jump_power = -3000
 			color_sprite.modulate = Color("green")
 			
+		CharacterColors.Blue: # Passthrough blue + blue related objects
+			bouncy = false
+			gravity = 20
+			light_node.visible = false
+			red_emitting = false
+			self.collision_mask = 1 | 2
+			jump_power = -2000
+			color_sprite.modulate = Color("blue")
+			
+		CharacterColors.Purple: # Passthrough with purple + purple related (intentionally confuse with blue)
+			bouncy = false
+			gravity = 20
+			light_node.visible = false
+			red_emitting = false
+			self.collision_mask = 1 | 3
+			jump_power = -2000
+			color_sprite.modulate = Color("Purple")
+			
 		CharacterColors.White: # Light source for dark rooms
-			point_light_2d.show()
+			bouncy = false
+			gravity = 20
+			light_node.visible = true
 			red_emitting = false
 			self.collision_mask = 1 | 2 | 3
 			jump_power = -2000
 			color_sprite.modulate = Color("white")
 			
-		CharacterColors.Black:
-			point_light_2d.get_parent().remove_child(point_light_2d)
-			hidden_node.add_child(point_light_2d)
+		CharacterColors.Black: # Hidden Block?
+			bouncy = false
+			gravity = 20
+			light_node.visible = false
 			red_emitting = false
 			self.collision_mask = 1 | 2 | 3
 			jump_power = -2000
